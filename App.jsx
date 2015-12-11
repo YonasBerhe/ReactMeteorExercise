e46ffe4a67dcda6180dbbb35cd2b4dc58e3720b9
Yonas Berhe <yonas.berhe007@gmail.com>
2015-12-11 01:10:21 -0800

    added form handler

--- App.jsx
+++ App.jsx
@@ -1,35 +1,34 @@
 App = React.createClass({
 
-mixins: [ReactMeteorData],
-
-
+  mixins: [ReactMeteorData],
 
   getMeteorData() {
-    return {
-      tasks: Task.find({}).fetch()
-    };
+    return {tasks: Task.find({}).fetch()};
   },
 
-renderTask () {
-  return this.getTasks().map((task) => {
-    return <Task key={task._id} task={task}/>;
-  });
-},
+  renderTask() {
+    return this.getTasks().map((task) => {
+      return <Task key={task._id} task={task}/>;
+    });
+  },
 
-render () {
-  return(
-    <div className="container">
-      <header>
-        <h1>
-          Todo List
-        </h1>
-      </header>
+  render() {
+    return (
+      <div className="container">
+        <header>
+          <h1>
+            Todo List
+          </h1>
+          {/* form to allow new data in to the Collection through the client side*/}
+          <form className="new-task" onSubmit={this.handleSubmit}>
+            <input type="text" ref="textInput" placeholder="Type to add new tasks"/>
+          </form>
+        </header>
+        <ul>
+          {this.renderTasks()}
+        </ul>
+      </div>
 
-      <ul>
-        {this.renderTasks()}
-      </ul>
-    </div>
-
-  );
-}
+    );
+  }
 });
