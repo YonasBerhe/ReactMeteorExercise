e151cbfbb0ba6875d766e969f9932b40dee54637
Yonas Berhe <yonas.berhe007@gmail.com>
2015-12-16 14:42:03 -0800

    fixed one little error

--- simple-todos-react.jsx
+++ simple-todos-react.jsx
@@ -13,14 +13,18 @@ if (Meteor.isClient) {
 }
 
 if (Meteor.isServer) {
-  Meteor.publish("tasks", function () {
-    return Tasks.find([
-      $or: [
-        {private: {$ne: true}},
-        {owner: this.userId}
-      ]
-    ]);
 
+
+
+  Meteor.publish("tasks", function () {
+
+
+    return Tasks.find({
+      $or: [
+             { private: {$ne: true} },
+             { owner: this.userId }
+           ]
+    });
   });
 }
 
