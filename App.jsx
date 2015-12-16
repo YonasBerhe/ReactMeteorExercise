040478ff84dd2301e74a005fa88adfbc1297128f
Yonas Berhe <yonas.berhe007@gmail.com>
2015-12-16 11:21:49 -0800

    mongo init

--- App.jsx
+++ App.jsx
@@ -1,48 +1,30 @@
 App = React.createClass({
 
-  mixins: [ReactMeteorData],
+mixins: [ReactMeteorData],
 
-  getMeteorData() {
-    return {tasks: Task.find({}).fetch()};
-  },
+getMeteorData(){
+  return {
+    tasks: Tasks.find({}).fetch()
+  }
+},
 
-  renderTask() {
-    return this.getTasks().map((task) => {
-      return <Task key={task._id} task={task}/>;
+  renderTasks() {
+    return this.data.tasks.map((task) => {
+      return <Task key={task._id} task={task} />;
     });
-  },
-
-  handleSubmit(event) {
-    event.preventDefault();
-    var text = React.findDOMNode(this.refs.textInput).value.trim();
-
-    // reffering to the mongo db tasks in the file simple todos react.js file
-    Tasks.insert({
-      text: text,
-      createdAt: new Date()
-    });
-
-    React.findDOMNode(this.refs.textInput).value = "";
-
   },
 
   render() {
     return (
       <div className="container">
         <header>
-          <h1>
-            Todo List
-          </h1>
-          {/* form to allow new data in to the Collection through the client side*/}
-          <form className="new-task" onSubmit={this.handleSubmit}>
-            <input type="text" ref="textInput" placeholder="Type to add new tasks"/>
-          </form>
+          <h1>Todo List</h1>
         </header>
+
         <ul>
           {this.renderTasks()}
         </ul>
       </div>
-
     );
   }
 });
